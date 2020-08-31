4d59edaa4eb795851671835676ea682f94e6b031
NicolasDominguez98 <nicodominguez@est.frba.utn.edu.ar>
2020-08-31 10:21:08 -0300

    Agregar UsuariosApiControllerTest

---
+++ tacs.api/src/test/java/tp/tacs/api/controllers/UsuariosApiControllerTest.java
@@ -0,0 +1,33 @@
+package tp.tacs.api.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import tp.tacs.api.model.Usuario;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+class UsuariosApiControllerTest {
+
+    @InjectMocks
+    private UsuariosApiController usuariosApiController;
+
+    @BeforeEach
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void listarUsuarios() {
+        ResponseEntity<List<Usuario>> usuariosResponse = usuariosApiController.listarUsuarios(0, null, 5);
+        assertEquals(HttpStatus.OK, usuariosResponse.getStatusCode());
+    }
+}
