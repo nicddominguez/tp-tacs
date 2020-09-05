3044d0380a5378032fd96b9c55858c1a7b2cdfab
Alejo Rodriguez <alejo.meson@gmail.com>
2020-09-05 16:46:59 -0300

    Implementacion de MunicipioReal

--- tacs.api/src/main/java/tp/tacs/api/dominio/Municipio.java
+++ tacs.api/src/main/java/tp/tacs/api/dominio/Municipio.java
@@ -1,4 +1,77 @@
 package tp.tacs.api.dominio;
 
 public class Municipio {
+
+    private Integer cantGauchos;
+    private boolean bloqueado;
+    private Especializacion especializacion;
+    private MunicipioReal municipioReal;
+
+    public Integer getCantGauchos() {
+        return cantGauchos;
+    }
+
+    public void setCantGauchos(Integer cantGauchos) {
+        this.cantGauchos = cantGauchos;
+    }
+
+    public boolean isBloqueado() {
+        return bloqueado;
+    }
+
+    public void setBloqueado(boolean bloqueado) {
+        this.bloqueado = bloqueado;
+    }
+
+    public Especializacion getEspecializacion() {
+        return especializacion;
+    }
+
+    public void setEspecializacion(Especializacion especializacion) {
+        this.especializacion = especializacion;
+    }
+
+    public MunicipioReal getMunicipioReal() {
+        return municipioReal;
+    }
+
+    public void setMunicipioReal(MunicipioReal municipioReal) {
+        this.municipioReal = municipioReal;
+    }
+
+    public Long getId() {
+        return this.municipioReal.getId();
+    }
+
+    public Double getLatitud() {
+        return this.municipioReal.getLatitud();
+    }
+
+    public Double getLongitud() {
+        return this.municipioReal.getLongitud();
+    }
+
+    public Integer getAltura() {
+        return this.municipioReal.getAltura();
+    }
+
+    public String getImagenPath() {
+        return this.municipioReal.getImagenPath();
+    }
+
+    public void producir() {
+        this.especializacion.producir(this);
+    }
+
+    public Municipio atacar(Municipio municipio) {
+        return null;
+    }
+
+    public Municipio moverGauchos(Municipio municipio, Integer cantidad) {
+        return null;
+    }
+
+    private void agregarGauchos(Integer cantGauchos) {
+        this.cantGauchos += cantGauchos;
+    }
 }
