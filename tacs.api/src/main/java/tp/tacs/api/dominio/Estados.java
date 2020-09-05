9c652f85d4c9dd92d99e4ae36587f1ba824de9fb
FrancoCirillo <francocirillo3@gmail.com>
2020-09-05 16:50:48 -0300

    Crear clases para la estructura del dominio

---
+++ tacs.api/src/main/java/tp/tacs/api/dominio/Estados.java
@@ -0,0 +1,7 @@
+package tp.tacs.api.dominio;
+
+public enum Estados {
+    EN_JUEGO,
+    TERMINADA,
+    CANCELADAS
+}
---
+++ tacs.api/src/main/java/tp/tacs/api/dominio/Usuario.java
@@ -0,0 +1,5 @@
+package tp.tacs.api.dominio;
+
+public class Usuario {
+    private String mail;
+}
