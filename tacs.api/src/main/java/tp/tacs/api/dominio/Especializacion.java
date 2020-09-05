4e370d33cdb5e1088fa54467055cdf6da6efcbd8
Alejo Rodriguez <alejo.meson@gmail.com>
2020-09-05 16:40:03 -0300

    Se agrega implementacion vacia de Especializacion

---
+++ tacs.api/src/main/java/tp/tacs/api/dominio/Especializacion.java
@@ -0,0 +1,6 @@
+package tp.tacs.api.dominio;
+
+public interface Especializacion {
+
+    public void producir(Municipio municipio);
+}
---
+++ tacs.api/src/main/java/tp/tacs/api/dominio/Produccion.java
@@ -0,0 +1,9 @@
+package tp.tacs.api.dominio;
+
+public class Produccion implements Especializacion {
+
+    @Override
+    public void producir(Municipio municipio) {
+
+    }
+}
