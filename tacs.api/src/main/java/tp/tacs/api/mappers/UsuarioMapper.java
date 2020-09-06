b344109a25a76cb7f2e6ad4c3dddf5556c7c5f1d
Alejo Rodriguez <alejo.meson@gmail.com>
2020-09-06 13:53:42 -0300

    Estadisticas del Usuario

---
+++ tacs.api/src/main/java/tp/tacs/api/mappers/UsuarioMapper.java
@@ -0,0 +1,13 @@
+package tp.tacs.api.mappers;
+
+import org.springframework.stereotype.Component;
+import tp.tacs.api.dominio.Usuario;
+import tp.tacs.api.model.UsuarioModel;
+
+@Component
+public class UsuarioMapper {
+
+    public UsuarioModel toModel(Usuario entity) {
+        return new UsuarioModel().id(entity.getId()).nombreDeUsuario(entity.getNombre());
+    }
+}
