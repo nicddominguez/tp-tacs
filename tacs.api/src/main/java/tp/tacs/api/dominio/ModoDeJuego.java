9b58bf9461b7764841688ee4da6450e06d3712d9
Alejo Rodriguez <alejo.meson@gmail.com>
2020-09-06 11:35:55 -0300

    format

--- tacs.api/src/main/java/tp/tacs/api/dominio/ModoDeJuego.java
+++ tacs.api/src/main/java/tp/tacs/api/dominio/ModoDeJuego.java
@@ -1,6 +1,8 @@
 package tp.tacs.api.dominio;
 
 public interface ModoDeJuego {
+
     Float getMultDist(Float distanciaMunicipios, Float minDist, Float maxDist);
+
     Float getMultAltura(Float alturaMunicipioDefensor, Float minAltura, Float maxAltura);
 }
