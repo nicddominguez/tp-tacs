08a7910ec820724f859f5cdbc618e1ae78426f85
FrancoCirillo <francocirillo3@gmail.com>
2020-09-06 11:53:29 -0300

    Crear clase (sin implementaciones) para obtener datos de Municipios

---
+++ tacs.api/src/main/java/tp/tacs/api/dominio/RepoMunicipios.java
@@ -0,0 +1,26 @@
+package tp.tacs.api.dominio;
+
+import java.util.List;
+
+public class RepoMunicipios {
+
+    List<Municipio> getMunicipios(String idProvincia, Integer cantidad) {
+        return null;
+    }
+
+    String getNombre(String idProvinca) {
+        return null;
+    }
+
+    Double getLatitud(String idProvinca) {
+        return null;
+    }
+
+    Double getLongitud(String idProvinca) {
+        return null;
+    }
+
+    Double getAltura(String idProvinca) {
+        return null;
+    }
+}
