ec789d4fcc296062f62fd9596e13a791477734af
Pablo Paglilla <pablopaglilla16@gmail.com>
2020-09-05 23:58:51 -0300

    Agregar atributos id y nombre a clase Usuario

--- tacs.api/src/main/java/tp/tacs/api/dominio/Usuario.java
+++ tacs.api/src/main/java/tp/tacs/api/dominio/Usuario.java
@@ -1,5 +1,29 @@
 package tp.tacs.api.dominio;
 
 public class Usuario {
+
+    private Long id;
+
     private String mail;
+
+    private String nombre;
+
+    public Usuario(Long id, String mail, String nombre) {
+        this.id = id;
+        this.mail = mail;
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
 }
