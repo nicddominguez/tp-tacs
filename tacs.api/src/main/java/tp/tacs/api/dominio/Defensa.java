9a75016f98d0bd32e914bdf3010ee734c56e8c63
Alejo Rodriguez <alejo.meson@gmail.com>
2020-09-06 11:35:50 -0300

    Implementacion nivel de produccion

--- tacs.api/src/main/java/tp/tacs/api/dominio/Defensa.java
+++ tacs.api/src/main/java/tp/tacs/api/dominio/Defensa.java
@@ -4,12 +4,18 @@ public class Defensa implements Especializacion {
 
     @Override
     public void producir(Municipio municipio) {
-        Integer cantGauchos = Math.round(10*(1-((municipio.getAltura() - municipio.getPartida().minAltura())/(2*(municipio.getPartida().maxAltura() - municipio.getPartida().minAltura())))));
+        Integer cantGauchos = this.nivelDeProduccion(municipio);
         municipio.agregarGauchos(cantGauchos);
     }
 
     @Override
     public Float multDensa() {
         return 1f;
+    }
+
+    @Override
+    public Integer nivelDeProduccion(Municipio municipio) {
+        return Math.round(10 * (1 - ((municipio.getAltura() - municipio.getPartida().minAltura())
+                / (2 * (municipio.getPartida().maxAltura() - municipio.getPartida().minAltura())))));
     }
 }
