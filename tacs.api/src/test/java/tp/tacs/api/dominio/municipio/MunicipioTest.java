07a5edab23e02c71da4f7230656037b4e5d47373
FrancoCirillo <francocirillo3@gmail.com>
2020-09-07 00:53:44 -0300

    Preparar tests municipio

--- tacs.api/src/test/java/tp/tacs/api/dominio/municipio/MunicipioTest.java
+++ tacs.api/src/test/java/tp/tacs/api/dominio/municipio/MunicipioTest.java
@@ -2,21 +2,45 @@ package tp.tacs.api.dominio.municipio;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.function.DoubleConsumer;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 class MunicipioTest {
+
+    @Mock
+    RepoMunicipios repoMunicipios;
+
+    Municipio municipioA;
+    Municipio municipioB;
+    Municipio municipioD;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
+        mockearRepoMunicipios("A", 100f, 10d, 0d);
+        mockearRepoMunicipios("B", 200f, 0d, 0d);
+        mockearRepoMunicipios("C", 0f, 30d, 40d);
+
+    }
+
+    private void mockearRepoMunicipios(String idProvincia, Float altura, Double latitud, Double longitud) {
+        when(repoMunicipios.getAltura(idProvincia)).thenReturn(altura);
+        when(repoMunicipios.getLatitud(idProvincia)).thenReturn(latitud);
+        when(repoMunicipios.getLongitud(idProvincia)).thenReturn(longitud);
     }
 
     @Test
-    void jugarPartida(){
+    void jugarPartida() {
+
 
     }
+
     @Test
     void producir() {
     }
