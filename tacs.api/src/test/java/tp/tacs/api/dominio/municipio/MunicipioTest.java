467051264b466b2c4ac2a5d8900eb0fe17851992
FrancoCirillo <francocirillo3@gmail.com>
2020-09-07 00:14:21 -0300

    Generada logica para jugar partida

---
+++ tacs.api/src/test/java/tp/tacs/api/dominio/municipio/MunicipioTest.java
@@ -0,0 +1,31 @@
+package tp.tacs.api.dominio.municipio;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MunicipioTest {
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void producir() {
+    }
+
+    @Test
+    void atacar() {
+    }
+
+    @Test
+    void ataqueExitoso() {
+    }
+
+    @Test
+    void moverGauchos() {
+    }
+}
