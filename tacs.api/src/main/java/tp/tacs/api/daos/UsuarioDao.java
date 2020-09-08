361c237380312ee70e58fbe6977979170bfc6b72
Pablo Paglilla <pablopaglilla16@gmail.com>
2020-09-08 01:58:36 -0300

    Agregar método UsuarioDao.findByGoogleId

--- tacs.api/src/main/java/tp/tacs/api/daos/UsuarioDao.java
+++ tacs.api/src/main/java/tp/tacs/api/daos/UsuarioDao.java
@@ -1,16 +1,26 @@
 package tp.tacs.api.daos;
 
+import org.springframework.stereotype.Component;
 import tp.tacs.api.dominio.usuario.Usuario;
 
 import java.util.*;
 
+@Component
 public class UsuarioDao implements Dao<Usuario> {
 
-    private Map<Long, Usuario> usuarios = new HashMap<>();
+    private final Map<Long, Usuario> usuarios = new HashMap<>();
 
     @Override
     public Usuario get(Long id) {
         return Optional.ofNullable(this.usuarios.get(id)).orElse(null);
+    }
+
+    public Optional<Usuario> getByGoogleId(String googleId) {
+        return this.usuarios
+                .values()
+                .stream()
+                .filter(usuario -> usuario.getGoogleId().equals(googleId))
+                .findFirst();
     }
 
     @Override
