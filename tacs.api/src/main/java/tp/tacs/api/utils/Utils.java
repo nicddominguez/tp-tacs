04e3a4240dc8c4c856e2f4df846793193d91f88f
Alejo Rodriguez <alejo.meson@gmail.com>
2020-09-08 12:23:02 -0300

    format Utils

--- tacs.api/src/main/java/tp/tacs/api/utils/Utils.java
+++ tacs.api/src/main/java/tp/tacs/api/utils/Utils.java
@@ -1,20 +1,20 @@
 package tp.tacs.api.utils;
 
-import org.springframework.http.ResponseEntity;
-import tp.tacs.api.model.UsuarioModel;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class Utils {
-    public <T> List<T> obtenerListaPaginada(Long pagina, Long tamanioPagina, List<T> lista){
+    public <T> List<T> obtenerListaPaginada(Long pagina, Long tamanioPagina, List<T> lista) {
         Long start = pagina * tamanioPagina;//23
         Long end = start + tamanioPagina;//23
 
-        if(end > lista.size()){
+        if (end > lista.size()) {
             end = (long) lista.size();
         }
 
-        if(start >= lista.size() || start < 0 || end <= 0)
+        if (start >= lista.size() || start < 0 || end <= 0)
             return null;
 
         return lista.subList(start.intValue(), end.intValue());
