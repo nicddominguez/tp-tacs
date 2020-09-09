e0265edfb6fe0fcf971ea0ea12c1022d19c50cd5
Alejo Rodriguez <alejo.meson@gmail.com>
2020-09-09 11:28:09 -0300

    Se borra comentario

--- tacs.api/src/main/java/tp/tacs/api/controllers/UsuariosApiController.java
+++ tacs.api/src/main/java/tp/tacs/api/controllers/UsuariosApiController.java
@@ -10,8 +10,6 @@ import tp.tacs.api.model.UsuarioModel;
 import tp.tacs.api.utils.Utils;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,20 +33,9 @@ public class UsuariosApiController implements UsuariosApi {
         List<UsuarioModel> usuarioModels =
                 usuarios.stream().map(usuario -> this.usuarioMapper.toModel(usuario)).collect(Collectors.toList());
 
-
-        /*
-        usuarioModels.add(new UsuarioModel().id(1L).nombreDeUsuario("Nico"));
-        usuarioModels.add(new UsuarioModel().id(1L).nombreDeUsuario("Franco"));
-        usuarioModels.add(new UsuarioModel().id(1L).nombreDeUsuario("Alejo"));
-        usuarioModels.add(new UsuarioModel().id(1L).nombreDeUsuario("Pablo"));
-        usuarioModels.add(new UsuarioModel().id(1L).nombreDeUsuario("Nico2"));
-        usuarioModels.add(new UsuarioModel().id(1L).nombreDeUsuario("Juan"));
-
-         */
-
         List<UsuarioModel> listaPaginada = this.utils.obtenerListaPaginada(pagina, tamanioPagina, usuarioModels);
 
-        if(listaPaginada == null)
+        if (listaPaginada == null)
             return ResponseEntity.notFound().build();
 
         return ResponseEntity.ok(new ListarUsuariosResponse().usuarios(listaPaginada));
