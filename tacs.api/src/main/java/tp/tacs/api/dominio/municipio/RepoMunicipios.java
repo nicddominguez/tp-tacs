f67a478c235650538d39c4815d67314d46e12a97
Alejo Rodriguez <alejo.meson@gmail.com>
2020-09-09 11:26:04 -0300

    Implementacion ProvinciasApiController

--- tacs.api/src/main/java/tp/tacs/api/dominio/municipio/RepoMunicipios.java
+++ tacs.api/src/main/java/tp/tacs/api/dominio/municipio/RepoMunicipios.java
@@ -1,5 +1,8 @@
 package tp.tacs.api.dominio.municipio;
 
+import tp.tacs.api.model.ProvinciaModel;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -45,5 +48,33 @@ public class RepoMunicipios {
 
     public String getPathImagen(String idMunicipioReal) {
         return "google.com";
+    }
+
+    public List<ProvinciaModel> getProvincias() {
+        List<ProvinciaModel> provinciaModels = new ArrayList<>();
+        provinciaModels.add(new ProvinciaModel().id(1L).nombre("Buenos Aires"));
+        provinciaModels.add(new ProvinciaModel().id(2L).nombre("Catamarca"));
+        provinciaModels.add(new ProvinciaModel().id(3L).nombre("Chaco"));
+        provinciaModels.add(new ProvinciaModel().id(4L).nombre("Chubut"));
+        provinciaModels.add(new ProvinciaModel().id(5L).nombre("Córdoba"));
+        provinciaModels.add(new ProvinciaModel().id(6L).nombre("Corrientes"));
+        provinciaModels.add(new ProvinciaModel().id(7L).nombre("Entre Ríos"));
+        provinciaModels.add(new ProvinciaModel().id(8L).nombre("Formosa"));
+        provinciaModels.add(new ProvinciaModel().id(9L).nombre("Jujuy"));
+        provinciaModels.add(new ProvinciaModel().id(10L).nombre("La Pampa"));
+        provinciaModels.add(new ProvinciaModel().id(11L).nombre("La Rioja"));
+        provinciaModels.add(new ProvinciaModel().id(12L).nombre("Mendoza"));
+        provinciaModels.add(new ProvinciaModel().id(13L).nombre("Misiones"));
+        provinciaModels.add(new ProvinciaModel().id(14L).nombre("Neuquén"));
+        provinciaModels.add(new ProvinciaModel().id(15L).nombre("Río Negro"));
+        provinciaModels.add(new ProvinciaModel().id(16L).nombre("Salta"));
+        provinciaModels.add(new ProvinciaModel().id(17L).nombre("San Juan"));
+        provinciaModels.add(new ProvinciaModel().id(18L).nombre("San Luis"));
+        provinciaModels.add(new ProvinciaModel().id(19L).nombre("Santa Cruz"));
+        provinciaModels.add(new ProvinciaModel().id(20L).nombre("Santa Fe"));
+        provinciaModels.add(new ProvinciaModel().id(21L).nombre("Santiago del Estero"));
+        provinciaModels.add(new ProvinciaModel().id(22L).nombre("Tierra del Fuego, Antártida e Isla del Atlántico Sur"));
+        provinciaModels.add(new ProvinciaModel().id(23L).nombre("Tucumán"));
+        return provinciaModels;
     }
 }
