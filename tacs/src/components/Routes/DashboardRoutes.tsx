d8b852426ae905068dba2120e4dbae31a7325f0c
NicolasDominguez98 <nicodominguez@est.frba.utn.edu.ar>
2020-09-12 16:39:08 -0300

    Agregar rutas para el Dashboard

---
+++ tacs/src/components/Routes/DashboardRoutes.tsx
@@ -0,0 +1,36 @@
+import BarChartIcon from "@material-ui/icons/BarChart";
+import HomeIcon from "@material-ui/icons/Home";
+import MapIcon from "@material-ui/icons/Map";
+import ViewListIcon from "@material-ui/icons/ViewList";
+import Home from "components/Dashboard/Home";
+import Partidas from "components/Partidas/Partidas";
+import React from "react";
+
+export const dashboardRoutes = [
+  {
+    path: "/app",
+    exact: true,
+    sidebar: "Home",
+    main: () => <Home />,
+    icon: () => <HomeIcon />,
+  },
+  {
+    path: "/app/partidas",
+    sidebar: "Partidas",
+    main: () => <Partidas />,
+    icon: () => <ViewListIcon />,
+  },
+  {
+    path: "/app/nuevaPartida",
+    exact: true,
+    sidebar: "Nueva Partida",
+    main: () => <h2>Nueva Partida</h2>,
+    icon: () => <MapIcon />,
+  },
+  {
+    path: "/app/estadisticas",
+    sidebar: "Estadísticas",
+    main: () => <h2>Estadisticas</h2>,
+    icon: () => <BarChartIcon />,
+  },
+];
