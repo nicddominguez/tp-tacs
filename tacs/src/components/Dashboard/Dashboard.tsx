1f3502f64ed0f2c319a2a67c685b6c8012c0a46a
NicolasDominguez98 <nicodominguez@est.frba.utn.edu.ar>
2020-09-12 16:41:17 -0300

    Integrar pagina de partidas al dashboard

---
+++ tacs/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,209 @@
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import MenuIcon from "@material-ui/icons/Menu";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import clsx from "clsx";
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import { dashboardRoutes as routes } from "components/Routes/DashboardRoutes";
+import { WololoAuthApiClient } from "api/client";
+import { UsuarioModel } from "api/api";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  toolbar: {
+    paddingRight: 24,
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: "none",
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(0),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(0),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[100]
+        : theme.palette.grey[900],
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto",
+  },
+}));
+
+
+export interface DashboardProps {
+  flagLoggedOut: () => void;
+  currentUser?: UsuarioModel;
+}
+
+
+const authApiClient = new WololoAuthApiClient();
+
+
+export default function Dashboard(props: DashboardProps) {
+  const classes = useStyles();
+
+  const [open, setOpen] = React.useState(true);
+
+  const doLogOut = () => {
+    authApiClient.logUserOut();
+    props.flagLoggedOut();
+  }
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        // TODO: El sidebar no tiene animacion al colapsarse o expandirse
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h5"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
+            Wololo Dashboard
+          </Typography>
+          {props.currentUser &&
+          <Typography
+            component="h2"
+            variant="h6"
+            color="inherit"
+            noWrap
+          >
+            {props.currentUser.nombreDeUsuario}
+          </Typography>
+          }
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="persistent"
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {routes.map((route) => (
+            <Link
+              to={route.path}
+              style={{ textDecoration: "none", color: "#444" }}
+            >
+              <ListItem button>
+                <ListItemIcon>{route.icon()}</ListItemIcon>
+                <ListItemText primary={route.sidebar} />
+              </ListItem>
+            </Link>
+          ))}
+          <ListItem button onClick={doLogOut} >
+            <ListItemIcon><ExitToAppIcon /></ListItemIcon>
+            <ListItemText primary="Cerrar Sesión" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            exact={route.exact}
+            component={route.main}
+          />
+        ))}
+      </main>
+    </div>
+  );
+}
