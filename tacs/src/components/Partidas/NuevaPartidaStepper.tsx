06c23553bebe2b57f06d4b38f20b6124065a6819
Pablo Paglilla <pablopaglilla16@gmail.com>
2020-09-13 20:08:33 -0300

    Consultar listado de provincias para crear partida al backend

--- tacs/src/components/Partidas/NuevaPartidaStepper.tsx
+++ tacs/src/components/Partidas/NuevaPartidaStepper.tsx
@@ -12,10 +12,12 @@ import StepLabel from "@material-ui/core/StepLabel";
 import Stepper from "@material-ui/core/Stepper";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import VolumeUp from "@material-ui/icons/VolumeUp";
+import {withStyles} from '@material-ui/core/styles'
 import { default as React } from "react";
+import { ProvinciaModel } from "api/api";
+import { WololoProvinciasApiClient } from "api/client";
 
-const useStyles = makeStyles((theme: Theme) =>
+const styles = (theme: Theme) =>
   createStyles({
     root: {
       width: "100%",
@@ -40,135 +42,286 @@ const useStyles = makeStyles((theme: Theme) =>
     input: {
       width: 42,
     },
-  })
+  }
 );
 
-function getSteps() {
-  return [
-    "Seleccionar una provincia",
-    "Seleccionar cantidad de municipios",
-    "Seleccionar modo de juego",
-    "Buscar jugadores",
-  ];
+const stepNames = [
+  "Seleccionar una provincia",
+  "Seleccionar cantidad de municipios",
+  "Buscar jugadores",
+];
+
+
+interface SelectorProvinciasProps {
+  provincias: Array<ProvinciaModel>
+  idProvinciaSeleccionada: number
+  onChange: (idProvincia: number) => void
+  classes?: any
 }
 
-export default function NuevaPartidaStepper() {
-  const classes = useStyles();
-  const steps = getSteps();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [provincia, setProvincia] = React.useState("");
-  const [modoDeJuego, setModoDeJuego] = React.useState("");
-  const [cantidadDeMunicipios, setCantidadDeMunicipios] = React.useState<
-    number | string | Array<number | string>
-  >(30);
+interface SelectorProvinciaState {
+  idProvincia: number
+}
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+class SelectorProvincia extends React.Component<SelectorProvinciasProps, SelectorProvinciaState> {
 
-  const handleProvincia = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setProvincia(event.target.value as string);
-  };
+  classes: any
 
-  const handleModoDeJuego = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setModoDeJuego(event.target.value as string);
-  };
+  constructor(props: SelectorProvinciasProps) {
+    super(props);
+    this.classes = this.props.classes;
 
-  const handleSliderChange = (event: any, newValue: number | number[]) => {
-    setCantidadDeMunicipios(newValue);
-  };
+    this.state = {
+      idProvincia: props.idProvinciaSeleccionada
+    };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCantidadDeMunicipios(
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  }
+
+  handleSelectChange(event: React.ChangeEvent<{ value: unknown }>) {
+    const idProvincia = event.target.value as number;
+    if (idProvincia) {
+      this.setState({
+        idProvincia: idProvincia
+      });
+      this.props.onChange(idProvincia);
+    }
+  }
+
+  render() {
+    return (
+      <FormControl className={this.classes.formControl}>
+      <InputLabel id="provincia-label">Provincia</InputLabel>
+      <Select
+        labelId="provincia-label"
+        id="provincia"
+        onChange={this.handleSelectChange}
+        value={this.state.idProvincia}
+      >
+        {this.props.provincias.map(provincia => (
+          <MenuItem key={provincia.id} value={provincia.id}>{provincia.nombre}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+    )
+  }
+
+}
+
+const SelectorProvinciaStyled = withStyles(styles)(SelectorProvincia);
+
+
+interface SelectorCantidadMunicipiosProps {
+  cantidadMunicipios: number | string | Array<number | string>
+  onChange: (cantidad: number | string | Array<number | string>) => void
+  classes?: any
+}
+
+
+class SelectorCantidadMunicipios extends React.Component<SelectorCantidadMunicipiosProps, { cantidadMunicipios: string | number | (string | number)[] }> {
+
+  classes: any
+
+  constructor(props: SelectorCantidadMunicipiosProps) {
+    super(props);
+    this.classes = this.props.classes;
+
+    this.state = {
+      cantidadMunicipios: props.cantidadMunicipios
+    };
+
+    this.setCantidadMunicipios = this.setCantidadMunicipios.bind(this);
+    this.handleSliderChange = this.handleSliderChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
+
+  setCantidadMunicipios(newValue: number | number[] | string) {
+    this.setState({
+      cantidadMunicipios: newValue
+    });
+    this.props.onChange(newValue);
+  }
+
+  handleSliderChange(event: any, newValue: number | number[]) {
+    this.setCantidadMunicipios(newValue);
+  }
+
+  handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setCantidadMunicipios(
       event.target.value === "" ? "" : Number(event.target.value)
     );
-  };
+  }
 
-  const handleBlur = () => {
-    if (cantidadDeMunicipios < 0) {
-      setCantidadDeMunicipios(0);
-    } else if (cantidadDeMunicipios > 100) {
-      setCantidadDeMunicipios(100);
+  handleBlur() {
+    if (this.state.cantidadMunicipios < 0) {
+      this.setCantidadMunicipios(0);
+    } else if (this.state.cantidadMunicipios > 100) {
+      this.setCantidadMunicipios(100);
     }
-  };
+  }
 
-  const getStepContent = (step: number) => {
-    switch (step) {
+  render() {
+    return (
+      <div className={this.classes.rootSlider}>
+      <Typography id="input-slider" gutterBottom>
+        Cantidad de municipios
+      </Typography>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs>
+          <Slider
+            value={typeof this.state.cantidadMunicipios === "number" ? this.state.cantidadMunicipios : 12}
+            onChange={this.handleSliderChange}
+            aria-labelledby="input-slider"
+            min={12}
+            max={50}
+          />
+        </Grid>
+        <Grid item>
+          <Input
+            className={this.classes.input}
+            value={typeof this.state.cantidadMunicipios === "number" ? this.state.cantidadMunicipios : 12}
+            margin="dense"
+            onChange={this.handleInputChange}
+            onBlur={this.handleBlur}
+            inputProps={{
+              step: 1,
+              min: 12,
+              max: 100,
+              type: "number",
+              "aria-labelledby": "input-slider",
+            }}
+          />
+        </Grid>
+      </Grid>
+    </div>
+    )
+  }
+
+}
+
+
+const SelectorCantidadMunicipiosStyled = withStyles(styles)(SelectorCantidadMunicipios);
+
+
+interface NuevaPartidaStepperState {
+  activeStep: number
+  idProvincia: number
+  provincias: Array<ProvinciaModel>
+  cantidadMunicipios: number | string | Array<number | string>
+  modoDeJuego: string
+}
+
+
+interface NuevaPartidaStepperProps {
+  classes?: any
+}
+
+
+export class NuevaPartidaStepper extends React.Component<NuevaPartidaStepperProps, NuevaPartidaStepperState> {
+
+  provinciasApiClient: WololoProvinciasApiClient
+  classes: any
+
+  constructor(props: NuevaPartidaStepperProps) {
+    super(props);
+    this.classes = props.classes;
+    this.provinciasApiClient = new WololoProvinciasApiClient();
+
+    this.state = {
+      activeStep: 0,
+      idProvincia: 1,
+      provincias: [],
+      cantidadMunicipios: 30,
+      modoDeJuego: ""
+    };
+
+    this.setProvincias = this.setProvincias.bind(this);
+    this.handleBack = this.handleBack.bind(this);
+    this.handleNext = this.handleNext.bind(this);
+    this.setIdProvincia = this.setIdProvincia.bind(this);
+    this.setCantidadMunicipios = this.setCantidadMunicipios.bind(this);
+  }
+
+  setProvincias(provincias: Array<ProvinciaModel>) {
+    this.setState({
+      provincias: provincias
+    });
+  }
+
+  componentDidMount() {
+    this.provinciasApiClient.listarProvincias(23)
+        .then(response => {
+          this.setProvincias(response.provincias || []);
+        })
+        .catch(console.error);
+  }
+
+  handleNext() {
+    this.setState(prevState => {
+      return {
+        activeStep: prevState.activeStep + 1
+      }
+    });
+  }
+
+  handleBack() {
+    this.setState(prevState => {
+      return {
+        activeStep: prevState.activeStep - 1
+      }
+    });
+  }
+
+  setIdProvincia(id: number) {
+    this.setState({
+      idProvincia: id
+    });
+  }
+
+  setCantidadMunicipios(cantidad: number | string | Array<number | string>) {
+    this.setState({
+      cantidadMunicipios: cantidad
+    })
+  }
+
+  handleModoDeJuego(event: React.ChangeEvent<{ value: unknown }>) {
+    this.setState({
+      modoDeJuego: event.target.value as string
+    });
+  }
+
+  renderActiveStep() {
+    switch (this.state.activeStep) {
       case 0:
         return (
-          <FormControl className={classes.formControl}>
-            <InputLabel id="provincia-label">Provincia</InputLabel>
-            <Select
-              labelId="provincia-label"
-              id="provincia"
-              value={provincia}
-              onChange={handleProvincia}
-            >
-              <MenuItem value={10}>Buenos Aires</MenuItem>
-              <MenuItem value={20}>Santa Fe</MenuItem>
-              <MenuItem value={30}>La Pampa</MenuItem>
-              <MenuItem value={40}>Cordoba</MenuItem>
-              <MenuItem value={50}>Entre Rios</MenuItem>
-              <MenuItem value={60}>Rio Negro</MenuItem>
-            </Select>
-          </FormControl>
+          <SelectorProvinciaStyled
+            onChange={this.setIdProvincia}
+            idProvinciaSeleccionada={this.state.idProvincia}
+            provincias={this.state.provincias}
+          />
         );
       case 1:
         return (
-          <div className={classes.rootSlider}>
-            <Typography id="input-slider" gutterBottom>
-              Cantidad de municipios
-            </Typography>
-            <Grid container spacing={2} alignItems="center">
-              <Grid item xs>
-                <Slider
-                  value={
-                    typeof cantidadDeMunicipios === "number"
-                      ? cantidadDeMunicipios
-                      : 0
-                  }
-                  onChange={handleSliderChange}
-                  aria-labelledby="input-slider"
-                  min={12}
-                  max={50}
-                />
-              </Grid>
-              <Grid item>
-                <Input
-                  className={classes.input}
-                  value={cantidadDeMunicipios}
-                  margin="dense"
-                  onChange={handleInputChange}
-                  onBlur={handleBlur}
-                  inputProps={{
-                    step: 1,
-                    min: 12,
-                    max: 50,
-                    type: "number",
-                    "aria-labelledby": "input-slider",
-                  }}
-                />
-              </Grid>
-            </Grid>
-          </div>
+          <SelectorCantidadMunicipiosStyled
+            onChange={this.setCantidadMunicipios}
+            cantidadMunicipios={this.state.cantidadMunicipios}
+          />
         );
       case 2:
         return (
-          <FormControl className={classes.formControl}>
+          <FormControl className={this.classes.formControl}>
             <InputLabel id="modoDeJuego-label">Modo de juego</InputLabel>
             <Select
               labelId="modoDeJuego-label"
               id="modoDeJuego"
-              value={modoDeJuego}
+              value={this.state.modoDeJuego}
               onChange={handleModoDeJuego}
             >
-              <MenuItem value={10}>Rápido</MenuItem>
-              <MenuItem value={20}>Normal</MenuItem>
-              <MenuItem value={30}>Extendido</MenuItem>
+              <MenuItem value={"RAPIDO"}>Rápido</MenuItem>
+              <MenuItem value={"NORMAL"}>Normal</MenuItem>
+              <MenuItem value={"EXTENDIDO"}>Extendido</MenuItem>
             </Select>
           </FormControl>
         );
@@ -177,32 +330,33 @@ export default function NuevaPartidaStepper() {
       default:
         return "Unknown step";
     }
-  };
+  }
 
-  return (
-    <div className={classes.root}>
-      <Stepper activeStep={activeStep} orientation="vertical">
-        {steps.map((label, index) => (
+  render() {
+    return (
+      <div className={this.classes.root}>
+      <Stepper activeStep={this.state.activeStep} orientation="vertical">
+        {stepNames.map((label, index) => (
           <Step key={label}>
             <StepLabel>{label}</StepLabel>
             <StepContent>
-              <Typography>{getStepContent(index)}</Typography>
-              <div className={classes.actionsContainer}>
+              <Typography>{this.renderActiveStep()}</Typography>
+              <div className={this.classes.actionsContainer}>
                 <div>
                   <Button
-                    disabled={activeStep === 0}
-                    onClick={handleBack}
-                    className={classes.button}
+                    disabled={this.state.activeStep === 0}
+                    onClick={this.handleBack}
+                    className={this.classes.button}
                   >
                     Back
                   </Button>
                   <Button
                     variant="contained"
                     color="primary"
-                    onClick={handleNext}
-                    className={classes.button}
+                    onClick={this.handleNext}
+                    className={this.classes.button}
                   >
-                    {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                    {this.state.activeStep === stepNames.length - 1 ? "Finish" : "Next"}
                   </Button>
                 </div>
               </div>
@@ -211,5 +365,10 @@ export default function NuevaPartidaStepper() {
         ))}
       </Stepper>
     </div>
-  );
+    )
+  }
+
 }
+
+
+export default withStyles(styles)(NuevaPartidaStepper);
