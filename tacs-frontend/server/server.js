6127b76d66b6e2bc99f600b20637d829628943b9
Pablo Paglilla <pablopaglilla16@gmail.com>
2020-09-24 02:41:06 -0300

    Permitir cors en servicio de frontend

--- tacs-frontend/server/server.js
+++ tacs-frontend/server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const maps = require('./maps');
 
 maps.initializeMapData();
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get(['/', '/app', '/app/*', '/login'], (req, res) => {
