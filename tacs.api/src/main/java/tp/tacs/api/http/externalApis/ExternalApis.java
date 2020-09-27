00aa885ba92e754ca90a55cfb30641d54bf89836
Nicolas Coen <ncoen97@gmail.com>
2020-09-27 20:18:14 -0300

    Conexion api imagenes

--- tacs.api/src/main/java/tp/tacs/api/http/externalApis/ExternalApis.java
+++ tacs.api/src/main/java/tp/tacs/api/http/externalApis/ExternalApis.java
@@ -3,14 +3,12 @@ package tp.tacs.api.http.externalApis;
 import com.google.gson.Gson;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 import tp.tacs.api.dominio.municipio.Municipio;
 import tp.tacs.api.http.HttpClientConnector;
-import tp.tacs.api.http.externalApis.models.MunicipiosApi;
-import tp.tacs.api.http.externalApis.models.Provincias;
-import tp.tacs.api.http.externalApis.models.TopoData;
-import tp.tacs.api.http.externalApis.models.TopoResult;
+import tp.tacs.api.http.externalApis.models.*;
 import tp.tacs.api.mappers.GeorefMapper;
 import tp.tacs.api.mappers.ProvinciaMapper;
 import tp.tacs.api.model.ProvinciaModel;
@@ -27,18 +25,21 @@ public class ExternalApis {
     private GeorefMapper geoRefWrapper;
     @Autowired
     private ProvinciaMapper provinciaWrapper;
-
+    @Value("${pixabay.key}")
+    private String pixabayKey;
     private String geoRefMunicipioBaseUrlEstandar = "https://apis.datos.gob.ar/georef/api/departamentos?aplanar=true";
     private String geoRefProvinciabaseUrlEstandar = "https://apis.datos.gob.ar/georef/api/provincias?aplanar=true";
     private String geoRefProvinciaNombre = "https://apis.datos.gob.ar/georef/api/provincias?aplanar=true&id=";
     private String topoBaseUrl = "https://api.opentopodata.org/v1/srtm90m?locations=";
+    private String pixabayUrl = "https://pixabay.com/api/?key=%s&image_type=photo&q=argentina+";
 
     @Cacheable("municipios")
-    public List<Municipio> getMunicipios(String idProvincia) {
+    public List<Municipio> getMunicipios(String idProvincia, String nombreProvincia) {
         String url = geoRefMunicipioBaseUrlEstandar + "&provincia=" + idProvincia + "&max=5000"; //Si no especificamos un max, georef no devuelve todos
         var municipiosApi = connector.get(url, MunicipiosApi.class);
         var municipiosBase = geoRefWrapper.wrapList(municipiosApi.getDepartamentos());
-        return getAlturas(municipiosBase);
+        var municipiosConImagenes = getImagenes(municipiosBase, nombreProvincia);
+        return getAlturas(municipiosConImagenes);
     }
 
     @SneakyThrows private List<Municipio> getAlturas(List<Municipio> municipios) {
@@ -52,8 +53,21 @@ public class ExternalApis {
         return municipios;
     }
 
-    public String getPathImagen(String idMunicipio) {
-        return "https://cdn.pixabay.com/photo/2020/02/16/19/41/horses-4854601_960_720.jpg";
+    @SneakyThrows private List<Municipio> getImagenes(List<Municipio> municipios, String nombreProvincia) {
+        String url = String.format(pixabayUrl, pixabayKey);
+        String provincia = nombreProvincia.replace(" ","+");
+        List<ImagenApi> imagenes = new Gson().fromJson(connector.get(url), ImagenesApi.class).getHits();
+        Integer cantidadImagenes = imagenes.size();
+        if(cantidadImagenes == 0){
+            imagenes = new Gson().fromJson(connector.get(url), ImagenesApi.class).getHits();
+        }
+        for(Integer i = 0; i < municipios.size(); i++) {
+            double numeroConComa =  (double)i / cantidadImagenes;
+            double parteDecimal = numeroConComa - Math.floor(numeroConComa);
+            double indiceImagen = (parteDecimal) * cantidadImagenes;
+            municipios.get(i).setUrlImagen(imagenes.get((int) indiceImagen).getLargeImageURL());
+        }
+        return municipios;
     }
 
     @SneakyThrows private ProvinciaModel agregarCantidadMunicipios(ProvinciaModel provincia) {
