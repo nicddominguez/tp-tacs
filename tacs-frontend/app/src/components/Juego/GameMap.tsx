db222390a45d60c31c445f5f3af3b62684c6a977
NicolasDominguez98 <nicodominguez@est.frba.utn.edu.ar>
2020-09-27 02:30:41 -0300

    Deshabilitar "Pasar turno" cuando no lo es

--- tacs-frontend/app/src/components/Juego/GameMap.tsx
+++ tacs-frontend/app/src/components/Juego/GameMap.tsx
@@ -1,175 +1,192 @@
-import React from 'react';
-import ReactDomServer from 'react-dom/server';
-import './map.css';
-import { Feature, Geometry, GeoJsonObject } from 'geojson';
-import { LatLng, Layer, PathOptions, LeafletMouseEvent } from 'leaflet';
-import { Map, GeoJSON, TileLayer, Marker, Popup } from 'react-leaflet';
-import Control from 'react-leaflet-control';
-import { UsuarioModel, MunicipioEnJuegoModel, PartidaModel } from 'api/api';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
+import React from "react";
+import ReactDomServer from "react-dom/server";
+import "./map.css";
+import { Feature, Geometry, GeoJsonObject } from "geojson";
+import { LatLng, Layer, PathOptions, LeafletMouseEvent } from "leaflet";
+import { Map, GeoJSON, TileLayer, Marker, Popup } from "react-leaflet";
+import Control from "react-leaflet-control";
+import { UsuarioModel, MunicipioEnJuegoModel, PartidaModel } from "api/api";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
 
 export interface GameMapProps {
-    mapData?: GeoJsonObject | Array<GeoJsonObject>
-    onClickMunicipio?: (municipio: MunicipioEnJuegoModel) => void
-    partida?: PartidaModel
-    usuarioLogueado?: UsuarioModel
-    onPasarTurno?: () => void
-    onTerminarPartida?: () => void
+  mapData?: GeoJsonObject | Array<GeoJsonObject>;
+  onClickMunicipio?: (municipio: MunicipioEnJuegoModel) => void;
+  partida?: PartidaModel;
+  usuarioLogueado?: UsuarioModel;
+  onPasarTurno?: () => void;
+  onTerminarPartida?: () => void;
 }
 
 interface GameMapState {
-    startLatitude: number,
-    startLongitude: number,
-    startZoom: number,
-    playerColors: { [k: number]: string }
+  startLatitude: number;
+  startLongitude: number;
+  startZoom: number;
+  playerColors: { [k: number]: string };
 }
 
-const GAME_MAP_COLORS = ['red', 'brown', 'green', 'yellow'];
+const GAME_MAP_COLORS = ["red", "brown", "green", "yellow"];
 
+export default class GameMap extends React.Component<
+  GameMapProps,
+  GameMapState
+> {
+  state: GameMapState;
 
-export default class GameMap extends React.Component<GameMapProps, GameMapState> {
-    state: GameMapState
+  constructor(props: GameMapProps) {
+    super(props);
 
-    constructor(props: GameMapProps) {
-        super(props);
+    let playerColors: { [k: number]: string } = {};
+    this.props.partida?.jugadores?.forEach((player, index) => {
+      playerColors[player.id] = GAME_MAP_COLORS[index];
+    });
 
-        let playerColors: { [k: number]: string } = {};
-        this.props.partida?.jugadores?.forEach((player, index) => {
-            playerColors[player.id] = GAME_MAP_COLORS[index]
-        });
+    this.state = {
+      startLatitude: -37.1315537735949,
+      startLongitude: -65.4466546606951,
+      startZoom: 5,
+      playerColors: playerColors,
+    };
 
-        this.state = {
-            startLatitude: -37.1315537735949,
-            startLongitude: -65.4466546606951,
-            startZoom: 5,
-            playerColors: playerColors
-        }
+    this.onEachFeature = this.onEachFeature.bind(this);
+    this.onClickArea = this.onClickArea.bind(this);
+    this.featureStyle = this.featureStyle.bind(this);
+    this.registerClickMunicipio = this.registerClickMunicipio.bind(this);
+  }
 
-        this.onEachFeature = this.onEachFeature.bind(this);
-        this.onClickArea = this.onClickArea.bind(this);
-        this.featureStyle = this.featureStyle.bind(this);
-        this.registerClickMunicipio = this.registerClickMunicipio.bind(this);
+  featureStyle(feature?: Feature<Geometry, any>): PathOptions {
+    const nombreMunicipio: string = feature?.properties.nombre;
+    const municipioEnJuego = this.props.partida?.informacionDeJuego?.municipios?.find(
+      (municipio) => municipio.nombre === nombreMunicipio
+    );
+
+    const idDuenio = municipioEnJuego?.duenio?.id;
+    return {
+      fillColor:
+        idDuenio !== undefined ? this.state.playerColors[idDuenio] : "grey",
+      fillOpacity: idDuenio !== undefined ? 0.5 : 0.2,
+    };
+  }
+
+  onEachFeature(feature: Feature<Geometry, any>, layer: Layer) {
+    layer.on({
+      click: this.onClickArea,
+    });
+  }
+
+  onClickArea(event: LeafletMouseEvent) {
+    const layer = event.target;
+    const nombre: string = layer.feature.properties.nombre;
+    this.props.partida?.informacionDeJuego?.municipios
+      ?.filter((municipio) => municipio.nombre === nombre)
+      .forEach(this.registerClickMunicipio);
+  }
+
+  registerClickMunicipio(municipio: MunicipioEnJuegoModel) {
+    if (this.props.onClickMunicipio) {
+      this.props.onClickMunicipio(municipio);
+    }
+  }
+
+  esElTurnoDelUsuario() {
+    if (
+      this.props.partida?.informacionDeJuego?.idUsuarioProximoTurno ===
+      undefined
+    ) {
+      return false;
     }
 
-    featureStyle(feature?: Feature<Geometry, any>): PathOptions {
-        const nombreMunicipio: string = feature?.properties.nombre;
-        const municipioEnJuego = this.props.partida?.informacionDeJuego
-            ?.municipios
-            ?.find(municipio => municipio.nombre === nombreMunicipio);
-
-        const idDuenio = municipioEnJuego?.duenio?.id;
-        return {
-            fillColor: idDuenio !== undefined ? this.state.playerColors[idDuenio] : "grey",
-            fillOpacity: idDuenio !== undefined ? 0.5 : 0.2
-        };
+    if (this.props.usuarioLogueado?.id === undefined) {
+      return false;
     }
 
-    onEachFeature(feature: Feature<Geometry, any>, layer: Layer) {
-        layer.on({
-            click: this.onClickArea
-        });
+    return (
+      this.props.partida?.informacionDeJuego?.idUsuarioProximoTurno ===
+      this.props.usuarioLogueado?.id
+    );
+  }
+
+  renderPopupMunicipio(municipio: MunicipioEnJuegoModel) {
+    if (municipio.ubicacion) {
+      return (
+        <Marker
+          position={
+            new LatLng(municipio.ubicacion.lat, municipio.ubicacion.lon)
+          }
+          onMouseOver={(e: LeafletMouseEvent) => {
+            e.target.openPopup();
+          }}
+          onMouseOut={(e: LeafletMouseEvent) => {
+            e.target.closePopup();
+          }}
+          onclick={() => this.registerClickMunicipio(municipio)}
+        >
+          <Popup>
+            {/* Acá se debería poder usar material sin problema! */}
+            <p>{municipio.nombre}</p>
+            <p>{municipio.gauchos}</p>
+          </Popup>
+        </Marker>
+      );
     }
+    return undefined;
+  }
 
-    onClickArea(event: LeafletMouseEvent) {
-        const layer = event.target;
-        const nombre: string = layer.feature.properties.nombre;
-        this.props.partida?.informacionDeJuego
-            ?.municipios
-            ?.filter(municipio => municipio.nombre === nombre)
-            .forEach(this.registerClickMunicipio);
-    }
+  render() {
+    const position: LatLng = new LatLng(
+      this.state.startLatitude,
+      this.state.startLongitude
+    );
+    return (
+      <Map center={position} zoom={this.state.startZoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {this.props.mapData && (
+          <GeoJSON
+            data={this.props.mapData}
+            onEachFeature={this.onEachFeature}
+            style={this.featureStyle}
+          />
+        )}
 
-    registerClickMunicipio(municipio: MunicipioEnJuegoModel) {
-        if (this.props.onClickMunicipio) {
-            this.props.onClickMunicipio(municipio);
-        }
-    }
+        {/* Mostramos un marker con popup */}
+        {this.props.partida?.informacionDeJuego?.municipios?.map(
+          this.renderPopupMunicipio
+        )}
 
-    esElTurnoDelUsuario() {
-        if (this.props.partida?.informacionDeJuego?.idUsuarioProximoTurno === undefined) {
-            return false;
-        }
+        {/* TODO: Acá hay que mostrar de quién es el turno actual. Se saca de la partida */}
+        {/* TODO: El botón de pasar turno se debería mostrar solo cuando es mi turno */}
+        {/* TODO: Poner botón de terminar partida */}
+        <Control position="bottomleft">
+          <Card>
+            <Button
+              onClick={
+                this.props.onPasarTurno ? this.props.onPasarTurno : () => {}
+              }
+              color="primary"
+              disabled={!this.esElTurnoDelUsuario()}
+            >
+              Pasar turno
+            </Button>
+          </Card>
+        </Control>
 
-        if (this.props.usuarioLogueado?.id === undefined) {
-            return false;
-        }
-
-        return this.props.partida?.informacionDeJuego?.idUsuarioProximoTurno === this.props.usuarioLogueado?.id;
-    }
-
-    renderPopupMunicipio(municipio: MunicipioEnJuegoModel) {
-        if(municipio.ubicacion) {
-            return (
-                <Marker
-                    position={new LatLng(municipio.ubicacion.lat, municipio.ubicacion.lon)}
-                    onMouseOver={(e: LeafletMouseEvent) => {
-                        e.target.openPopup();
-                    }}
-                    onMouseOut={(e: LeafletMouseEvent) => {
-                        e.target.closePopup();
-                    }}
-                    onclick={() => this.registerClickMunicipio(municipio)}
-                >
-                    <Popup>
-                        {/* Acá se debería poder usar material sin problema! */}
-                        <p>{municipio.nombre}</p>
-                        <p>{municipio.gauchos}</p>
-                    </Popup>
-                </Marker>
-            )
-        }
-        return undefined;
-    }
-
-    render() {
-        const position: LatLng = new LatLng(this.state.startLatitude, this.state.startLongitude);
-        return (
-            <Map center={position} zoom={this.state.startZoom} >
-                <TileLayer
-                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {this.props.mapData &&
-                    <GeoJSON
-                        data={this.props.mapData}
-                        onEachFeature={this.onEachFeature}
-                        style={this.featureStyle}
-                    />
-                }
-
-                {/* Mostramos un marker con popup */}
-                {this.props.partida?.informacionDeJuego?.municipios?.map(this.renderPopupMunicipio)}
-
-                {/* TODO: Acá hay que mostrar de quién es el turno actual. Se saca de la partida */}
-                {/* TODO: El botón de pasar turno se debería mostrar solo cuando es mi turno */}
-                {/* TODO: Poner botón de terminar partida */}
-                <Control position='bottomleft'>
-                    <Card>
-                        <Button
-                            onClick={this.props.onPasarTurno ? this.props.onPasarTurno : () => { }}
-                            color='primary'
-                        >
-                            Pasar turno
-                        </Button>
-                    </Card>
-                </Control>
-
-                {/* Para mostrar los jugadores y sus colores */}
-                <Control position='bottomright'>
-                    <Card>
-                        {this.props.partida?.jugadores.map(jugador => {
-                            return (
-                                <div>
-                                    <span>{this.state.playerColors[jugador.id]} </span>
-                                    <span>{jugador.nombreDeUsuario}</span>
-                                </div>
-                            );
-                        })}
-                    </Card>
-                </Control>
-            </Map>
-        )
-    }
-
+        {/* Para mostrar los jugadores y sus colores */}
+        <Control position="bottomright">
+          <Card>
+            {this.props.partida?.jugadores.map((jugador) => {
+              return (
+                <div>
+                  <span>{this.state.playerColors[jugador.id]} </span>
+                  <span>{jugador.nombreDeUsuario}</span>
+                </div>
+              );
+            })}
+          </Card>
+        </Control>
+      </Map>
+    );
+  }
 }
