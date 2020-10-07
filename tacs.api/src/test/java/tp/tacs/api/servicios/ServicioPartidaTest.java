fd18bbeb72b6e9e88163dddfdb5bf63c24a3d498
FrancoCirillo <francocirillo3@gmail.com>
2020-10-07 12:02:43 -0300

    Creación de la clase ServicioPartidaTest
    Porque a alejo no le dejaba crear la carpeta :(

---
+++ tacs.api/src/test/java/tp/tacs/api/servicios/ServicioPartidaTest.java
@@ -0,0 +1,121 @@
+package tp.tacs.api.servicios;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ServicioPartidaTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void validarAccion() {
+    }
+
+    @Test
+    void terminarPartida() {
+    }
+
+    @Test
+    void usuarioConMasMunicipios() {
+    }
+
+    @Test
+    void repartirMunicipios() {
+    }
+
+    @Test
+    void eliminarPerdedores() {
+    }
+
+    @Test
+    void pasarTurno() {
+    }
+
+    @Test
+    void hayGanador() {
+    }
+
+    @Test
+    void desbloquearYProducirMunicipios() {
+    }
+
+    @Test
+    void distanciaEntre() {
+    }
+
+    @Test
+    void multDistancia() {
+    }
+
+    @Test
+    void multAltura() {
+    }
+
+    @Test
+    void gauchosDefensoresFinales() {
+    }
+
+    @Test
+    void gauchosAtacantesFinales() {
+    }
+
+    @Test
+    void calcularMultAlturaMunicipio() {
+    }
+
+    @Test
+    void distintoDuenio() {
+    }
+
+    @Test
+    void calcularDistancias() {
+    }
+
+    @Test
+    void calcularAlturas() {
+    }
+
+    @Test
+    void simularAtacarMunicipio() {
+    }
+
+    @Test
+    void obtenerPartidaPorId() {
+    }
+
+    @Test
+    void moverGauchos() {
+    }
+
+    @Test
+    void inicializar() {
+    }
+
+    @Test
+    void atacar() {
+    }
+
+    @Test
+    void actualizarEstadoPartida() {
+    }
+
+    @Test
+    void asignarProximoTurno() {
+    }
+
+    @Test
+    void idUsuarioEnTurnoActual() {
+    }
+
+    @Test
+    void perteneceALaPartida() {
+    }
+}
